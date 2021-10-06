a20cc44a401b108d007bf2b6bddb5aaedb501a93
Leouna163 <91170703+mian9116@users.noreply.github.com>
2021-10-06 11:58:07 +0800

    Add files via upload

---
+++ week4/4-1.cpp
@@ -0,0 +1,10 @@
+#include<stdio.h>
+
+int main()
+
+{
+    int a=123,b=10;
+
+    int ans=a/b;
+    printf("%d\n",ans);
+}
---
+++ week4/4-2.cpp
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main()
+
+{
+    int a=123,b=10;
+
+    int ans1=a/b;
+
+    float ans2=123.0/10.0;
+
+    printf("%d\n",ans1);
+    printf("%f\n",ans2);
+}
